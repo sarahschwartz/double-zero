e9d8d7897522b5a40922c6dae246083c00737778
calvo.generico <miguelfeliped@gmail.com>
2024-11-20 18:02:59 -0300

    feat(proxy): account public actions
    Added restrictions for all account module public actions in public api

--- packages/proxy/src/routes/public-api-routes/account-handlers.ts
+++ packages/proxy/src/routes/public-api-routes/account-handlers.ts
@@ -1,12 +1,20 @@
-import { Handler } from './handlers.js';
+import { defaultHandler, Handler } from './handlers.js';
 import { z } from 'zod';
 import { addressSchema } from '../../utils/schemas.js';
 import { isAddressEqual } from 'viem';
 import { getUserOrThrow } from '../../services/user.js';
 import { buildUrl } from '../../utils/url.js';
+import { HttpError } from '../../utils/http-error.js';
 
 const withAddressSchema = z.object({
   address: addressSchema,
+});
+
+const withManyAddressesSchema = z.object({
+  address: z
+    .string()
+    .transform((s) => s.split(',').map((s) => s.trim()))
+    .pipe(z.array(addressSchema)),
 });
 
 const publicTxSchema = z.union([
@@ -85,6 +93,73 @@ const txlist: Handler = async (app, req, reply) => {
   return reply.send(filtered);
 };
 
+/**
+ * For this action we do the simplest possible check. Tx has to be from or to current user.
+ * @param app
+ * @param req
+ * @param reply
+ */
+const txlistinternal: Handler = async (app, req, reply) => {
+  const user = getUserOrThrow(req);
+
+  const url = buildUrl(`${app.conf.proxyTarget}/api`, {
+    ...req.query,
+    page: 1,
+    offset: 1000,
+  });
+
+  const data = await fetch(url)
+    .then((res) => res.json())
+    .then(publicTxSchema.parse);
+
+  if (data.status === '0') {
+    return reply.send(data);
+  }
+
+  const result = data.result.filter(
+    (tx) => isAddressEqual(tx.to, user) || isAddressEqual(tx.from, user),
+  );
+  const filtered = {
+    ...data,
+    result: paginate(result, req.query.page, req.query.offset),
+  };
+
+  return reply.send(filtered);
+};
+
+const passIfAddressIsCurrentUser: Handler = async (app, req, reply) => {
+  const user = getUserOrThrow(req);
+  const address = withAddressSchema.parse(req.query).address;
+
+  if (!isAddressEqual(user, address)) {
+    throw new HttpError('Only own address can be checked', 403);
+  }
+  return defaultHandler(app, req, reply);
+};
+
+const balancemulti: Handler = async (app, req, reply) => {
+  const user = getUserOrThrow(req);
+  const addresses = withManyAddressesSchema.parse(req.query).address;
+
+  if (!addresses.every((a) => isAddressEqual(a, user))) {
+    throw new HttpError('Only own address can be checked', 403);
+  }
+
+  return defaultHandler(app, req, reply);
+};
+const balance = passIfAddressIsCurrentUser;
+const tokenbalance = passIfAddressIsCurrentUser;
+const tokentx = passIfAddressIsCurrentUser;
+const tokennfttx = passIfAddressIsCurrentUser;
+const getminedblocks = passIfAddressIsCurrentUser;
+
 export const accountHandlers = {
   txlist,
+  txlistinternal,
+  balance,
+  balancemulti,
+  tokenbalance,
+  tokentx,
+  tokennfttx,
+  getminedblocks,
 };
